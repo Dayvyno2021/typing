17527dcc8ef94e8ae8889ed699381a23097be29d
Okafor David <dokafor@theshippack.com>
2024-02-24 16:44:34 +0100

    feat: Completed the typing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    const parsedValue = (<HTMLInputElement>event.target).value;
+    this.enteredText = parsedValue;
+  }
+
+  compare(randomLetter:string, enteredLetter:string) {
+    return !enteredLetter? 'has-text-grey-light': randomLetter===enteredLetter && enteredLetter? 'has-text-success':'has-text-danger'
+  }
 }
